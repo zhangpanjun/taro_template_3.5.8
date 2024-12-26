cfd2d7ab412fe60c5712f49d33b33cfde221a8c7
zhangpanjun <zhangpanjun93@163.com>
2024-12-26 15:24:25 +0800

    增加react组件

---
+++ src/components/reactComponents/Demo.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Demo = () => {
+    return (
+        <div>
+            我是Demo组件啥都不吃v还是打不出大撒把吃吧手打冰川时代不错的时间成本时擦等哈时擦啥都不吃死得很惨不大好是吧吃撒不差巴萨打几把成绩手打bBHCDBS
+        </div>
+    );
+};
+export default Demo;
